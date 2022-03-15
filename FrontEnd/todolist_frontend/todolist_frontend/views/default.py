89eaf1dde4d65b6d91134ec65caa265cc4f90ee7
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-03-15 03:24:44 +0200

    Front end Created with Route Mapping and Item Loop

--- FrontEnd/todolist_frontend/todolist_frontend/views/default.py
+++ FrontEnd/todolist_frontend/todolist_frontend/views/default.py
@@ -1,13 +1,18 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='todolist_frontend:templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'ToDoList_FrontEnd'}
+@view_config(route_name='home', renderer='todolist_frontend:templates/home.jinja2')
+def home(request ):
+    context ={'title':'Welcome Page',}
+    return context
 
+@view_config(route_name='listitem', renderer='todolist_frontend:templates/home.jinja2')
+def listitem(request):
+    print("hello")
+    ref = request.params['q']
+    items= ["item2","item3","item4","item5",ref]
+    context ={'title':'Welcome Page','items':items}
+    return context
+    
 
-
-@view_config(route_name='hello', renderer='todolist_frontend:templates/hello.jinja2')
-def hello(request):
-    return {'hello': 'Hello World'}
 
