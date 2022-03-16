7b80302914ab092aaf2f531cb6d25892be550e0c
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-03-16 03:28:56 +0200

    Backend Service Created

---
+++ BackEnd/index.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify,request
+import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/MyToDoList'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'a15s#%^a1A@Q$'
+
+db = SQLAlchemy(app)
+
+class Todolist(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    item=db.Column(db.String(100))
+
+    def __init__(self,item):
+        self.item = item
+
+
+@app.route("/search", methods=['GET'])
+def Search_to_do_items():
+    if request.method == 'GET':
+        request_data = request.get_json() 
+        reference = request_data["reference"]  
+        respond =[]
+        try:
+            response = Todolist.query.filter(Todolist.item.like('%{}%'.format(reference)))
+            for result in response:
+                value = {'item':result.item}
+                respond.append(value)
+            context = json.dumps(respond, indent=2)
+        except:
+            response = "No Data"
+            context = json.dumps(response, indent=2)
+    return context
+
+
+@app.route("/addToDoItem", methods=['POST'])
+def Add_to_do_items():
+    if request.method == 'POST':
+        try:
+            request_data = request.get_json()
+            item_data = request_data["item"]        
+            entry = Todolist(item_data)
+            db.session.add(entry)
+            db.session.commit()
+            response =  "Record Added"
+        except:    
+            response =  "Record Not Added"
+    return jsonify({'tst':response})
+
+if __name__ == '__main__':
+    # db.create_all()
+    app.run(debug=True)
